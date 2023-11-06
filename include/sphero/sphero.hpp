3eb89cb53d57e11095b866e667410fe782ece393
NANesseth <nikolai.astad@gmail.com>
2023-11-06 09:37:37 +0100

    Created library header file
    (cherry picked from commit f83106a86bf7768748226c762ee25153ee5a270b)

---
+++ include/sphero/sphero.hpp
@@ -0,0 +1,12 @@
+#ifndef AIS2203_PROJECT_SPHERO_SPHERO_HPP
+#define AIS2203_PROJECT_SPHERO_SPHERO_HPP
+
+#include "cameras/RaspberryCamera.hpp"
+
+#include "controls/RobotControls.hpp"
+
+#include "math/MathUtils.hpp"
+
+#include "utils/ThreadPool.hpp"
+
+#endif//AIS2203_PROJECT_SPHERO_SPHERO_HPP
